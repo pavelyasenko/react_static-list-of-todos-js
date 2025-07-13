a508fe04160f0058596215bf93c58ffd7b96cde4
Yatsenko Pavlo <pavelyasenko18@gmail.com>
2025-07-13 22:19:01 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.scss';
 import todosFromServer from './api/todos.json';
 import usersFromServer from './api/users.json';
+import { TodoList } from './components/TodoList/TodoList';
 
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) || null;
@@ -13,6 +14,6 @@ export const todos = todosFromServer.map(todo => ({
 
 export const App = () => (
   <div className="App">
-    <h1 className="App__title">Static list of todos</h1>
+    <TodoList todos={todos} />
   </div>
 );
